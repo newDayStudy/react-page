00817f2267dc624b29434d33443f5431ab386a6e
gushiyuan <429382356@qq.com>
2019-12-26 15:18:58 +0800

    'build文件'

---
+++ build/precache-manifest.b261b570f8ce34eebd41b416e928fd6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a290a205de8039e65a5746f2658f66d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b060b8ffcc9011e7311",
+    "url": "/static/css/2.aef8ad04.chunk.css"
+  },
+  {
+    "revision": "5f8476fc6b05a8178ce6",
+    "url": "/static/css/main.40b88f7f.chunk.css"
+  },
+  {
+    "revision": "0b060b8ffcc9011e7311",
+    "url": "/static/js/2.5b4abba6.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.5b4abba6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5f8476fc6b05a8178ce6",
+    "url": "/static/js/main.0a22719d.chunk.js"
+  },
+  {
+    "revision": "fbaf048f868100b869bd",
+    "url": "/static/js/runtime-main.8f658356.js"
+  }
+]);
